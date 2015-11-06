3b97cfab1cd58388e0f57d2ec049a98fa6a53595
Todd Crone <todd.crone@perceptivesoftware.com>
2015-11-05 21:15:08 -0500

    Add Idea plugin and @Unroll to test

---
+++ src/test/groovy/StockProblemSpec.groovy
@@ -0,0 +1,14 @@
+class StockProblemSpec extends spock.lang.Specification {
+  
+  @Unroll
+  def "length of Spock's and his friends' names"() {
+    expect:
+    name.size() == length
+
+    where:
+    name     | length
+    "Spock"  | 5
+    "Kirk"   | 4
+    "Scotty" | 6
+  }
+}  
