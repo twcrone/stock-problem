9bbe61daaca4a2136011bcb9efeadfe68a4ba51f
Todd Crone <todd.crone@perceptivesoftware.com>
2015-11-11 08:23:54 -0500

    Add no gains data

--- src/test/groovy/StockProblemSpec.groovy
+++ src/test/groovy/StockProblemSpec.groovy
@@ -4,7 +4,7 @@ import spock.lang.Unroll
 class StockProblemSpec extends Specification {
 
     static SIMPLE = [1.00, 2.00, 3.00, 4.00, 5.00]
-
+    static NO_GAINS = [5.00, 4.00, 3.00, 2.00, 1.00]
 
     @Unroll
     def "for stock data #data, you should buy at #buy, sell at #sell and your gains are #gains"() {
@@ -19,8 +19,9 @@ class StockProblemSpec extends Specification {
 
         where:
 
-        data   || buy   | sell  | gains
-        SIMPLE || 1.00  | 5.00  | 4.00
+        data        || buy   | sell  | gains
+        SIMPLE      || 1.00  | 5.00  | 4.00
+        NO_GAINS    || 0.00  | 0.00  | 0.00
     }
 
     def solveFor(data) {
