180dad77ab0f60070c5f048cce9a8ee5c1655e46
Todd Crone <todd.crone@perceptivesoftware.com>
2015-11-11 08:21:17 -0500

    Add brute force solution

--- src/test/groovy/StockProblemSpec.groovy
+++ src/test/groovy/StockProblemSpec.groovy
@@ -10,21 +10,33 @@ class StockProblemSpec extends Specification {
     def "for stock data #data, you should buy at #buy, sell at #sell and your gains are #gains"() {
 
         when:
-        def (actualBuy, actualSell, actualGains) = solveFor(data)
+        def solution = solveFor(data)
 
         then:
-        actualBuy == buy
-        actualSell == sell
-        actualGains == gains
+        solution.buy == buy
+        solution.sell == sell
+        solution.gains == gains
 
         where:
 
         data   || buy   | sell  | gains
-        SIMPLE || 1.00  | 2.00  | 1.00
+        SIMPLE || 1.00  | 5.00  | 4.00
     }
 
     def solveFor(data) {
-        [1.0, 2.0, 1.0]
+        def solution = [buy: 0, sell: 0, gains:0]
+
+        for(int i = 0; i < data.size(); ++i) {
+            for(int j = i + 1; j < data.size(); ++j) {
+                def sell = data[j]
+                def buy = data[i]
+                def gains = data[j] - data[i]
+                if(solution.gains < gains) {
+                    solution = [buy: buy, sell: sell, gains: gains]
+                }
+            }
+        }
+        solution
     }
 
 }
