da007a826c8d46d72432331894885e2bcdb53d18
Todd Crone <todd.crone@perceptivesoftware.com>
2015-11-11 08:09:10 -0500

    Remove early optimizations

--- src/test/groovy/StockProblemSpec.groovy
+++ src/test/groovy/StockProblemSpec.groovy
@@ -5,6 +5,7 @@ class StockProblemSpec extends Specification {
 
     static SIMPLE = [1.00, 2.00, 3.00, 4.00, 5.00]
 
+
     @Unroll
     def "for stock data #data, you should buy at #buy, sell at #sell and your gains are #gains"() {
 
@@ -22,61 +23,10 @@ class StockProblemSpec extends Specification {
         SIMPLE || 1.00  | 2.00  | 1.00
     }
 
-    @Unroll
-    def "for stock data #data, price at #time is a min value is #expected"() {
-
-        expect:
-        isMin(data, t) == expected
-
-        where:
-
-        data    |   t   ||  expected
-        SIMPLE  |   0   ||  true
-        SIMPLE  |   2   ||  false
-        SIMPLE  |   4   ||  false
-    }
-
-    @Unroll
-    def "for stock data #data, price at #time is a max value is #expected"() {
-
-        expect:
-        isMax(data, t) == expected
-
-        where:
-
-        data    |   t   ||  expected
-        SIMPLE  |   0   ||  true
-        SIMPLE  |   2   ||  false
-        SIMPLE  |   4   ||  true
-    }
-
     def solveFor(data) {
         [1.0, 2.0, 1.0]
     }
 
-    def isMin(data, t) {
-        if(t - 1 < 0) {
-            true
-        }
-        else if(t + 1 >= data.size()) {
-            false
-        }
-        else if(data[t] < data[t -1] && data[t] < data[t + 1]) {
-            true
-        }
-        else { false }
-    }
-
-    def isMax(data, t) {
-        def size = data.size()
-        if(t + 1 >= size || t -1 < 0) {
-            true
-        }
-        else if(data[t] > data[t -1] && data[t] > data[t + 1]) {
-            true
-        }
-        else { false }
-    }
 }
 
 
